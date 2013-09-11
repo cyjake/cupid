696f386dda896651a6c27341decd701116fff23f
Chen Yicai <yicai.cyj@taobao.com>
2013-09-11 13:32:04 +0800

    Enhanced check when adding feed

--- public/javascripts/feed-new.js
+++ public/javascripts/feed-new.js
@@ -1,4 +1,4 @@
-KISSY.use('ajax,node,event,sizzle', function(S, IO, Node) {
+KISSY.use('ajax,node,json,event,sizzle', function(S, IO, Node, JSON) {
 
   S.one('form').on('submit', function(e) {
     var form = Node(e.currentTarget)
@@ -16,10 +16,25 @@ KISSY.use('ajax,node,event,sizzle', function(S, IO, Node) {
       author: S.one('[name="author"]').val()
     }
 
-    IO.post('/feeds', data, function(res, status, io) {
-      alert(res.message)
+    function handler(res, status, io) {
+      if (res) {
+        alert(res.message)
+      }
+      else {
+        res = JSON.parse(io.responseText)
+        alert(res.message)
+      }
 
       if (io.status < 300) location.href = '/'
+    }
+
+    IO({
+      url: '/feeds',
+      type: 'post',
+      data: data,
+      dataType: 'json',
+      success: handler,
+      error: handler
     })
   })
 })
