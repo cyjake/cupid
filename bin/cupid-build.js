cbec8d4b8620993bfd30d0b76ec22b37ef8c1f4d
逸才 <yicai.cyj@taobao.com>
2016-11-22 14:53:12 +0800

    Breaking: src & target

--- bin/cupid-build.js
+++ bin/cupid-build.js
@@ -8,6 +8,18 @@ const program = require('commander')
 
 const Planet = require('../lib/planet')
 
+program
+  .option('-d, --destination [dir]', 'Destination directory (defaults to ./target)', 'target')
+
+program.on('--help', function() {
+  console.log(`
+  Examples:
+
+  $ cupid-build
+  $ cupid-build some-planet -d /path/to/webroot
+`.trim())
+})
+
 program.parse(process.argv)
 
 const cwd = process.cwd()
@@ -20,7 +32,7 @@ co(function* () {
   const planet = new Planet(root)
 
   yield planet.parse()
-  yield planet.write()
+  yield planet.write(program.destination)
 })
   .catch(function(err) {
     console.error(`Failed to build planet at [${root}]: ${err.stack}`)
